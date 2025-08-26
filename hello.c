e1c41601bedaaa8fe76f347e3b17c0d35b97d50a
360Js-UNL <jsplichal5@huskers.unl.edu>
2025-08-26 16:09:57 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * Author: Jonah Splichal
+ * Date: 2025-08-26
+ *
+ * Purpose: Prints my name into the default terminal. Also using for testing a repository
+ */
+
+ int main(int argc, char **argv){
+    printf("Jonah Splichal");
+    
+ }
